f3e5f31643092e4517958515aebf75a9e221dc3a
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-04 19:17:39 -0700

    step 8

--- server/messagesCtrl.js
+++ server/messagesCtrl.js
@@ -2,11 +2,22 @@ const allMessages = []
 
 module.exports = {
     getAllMessages: (req, res) => {
-        res.status(200),send(allMessages)
+        res.status(200), send(allMessages)
     },
-    createMessage: (req, res)=> {
-        const {username, message} = req.body
-        allMessages.push({username, message})
+    createMessage: (req, res) => {
+        const { username, message } = req.body
+        allMessages.push({ username, message })
+
+        if (req.session.history) {
+            req.session.history.push(username, message)
+        } else {
+            req.session.history = []
+            req.session.history.push(username, message)
+        }
+
         res.status(200).send(allMessages)
+    }, 
+    history: (req, res) => {
+        res.status.send(req.session.history)
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 require('dotenv').config()
 const express = require('express')
-// const sessions = require('express-session')
-const {SERVER_PORT} = process.env
+const session = require('express-session')
+const {SERVER_PORT, SESSION_SECRET} = process.env
 const ctrl = require('./messagesCtrl')
 
 const app = express()
 
 //middleware
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}))
 
 //endpoints
 app.get('/api/messages', ctrl.getAllMessages)
+app.post('/api/messages', ctrl.createMessage)
+app.get('/api/message', ctrl.history)
 
 //listening
 app.listen(SERVER_PORT, console.log(`testing, testing, port ${SERVER_PORT}`))
